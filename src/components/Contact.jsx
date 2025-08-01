4873950268ec989d14ca99f5e4869e5a5feddd3d
71762208015 <71762208015@cit.edu.in>
2025-08-01 12:12:29 +0530

    tw

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => (
     <h2 className="text-2xl font-bold mb-2">Contact Me</h2>
     <p>Email: gokulponraj30@gmail.com</p>
     <p>Phone: 9840676328</p>
-    <p>LinkedIn: <a className="text-blue-500" href="https://linkedin.com/in/gokul-p-20ba57292/" target="_blank">View Profile</a></p>
+    <p>LinkedIn: <a className="text-blue-500" href="https://linkedin.com/in/gokul-p-20ba57292/" target="_blank" rel="noreferrer">View Profile</a></p>
   </section>
 );
 export default Contact;
